6d4d9c49c57a3eb05c855ead5a4daa0caac632c8
Vu Nguyen <vu.nguyen@dmttek.com>
2025-08-09 23:32:37 +0700

    close news modal on pop state

--- src/news/NewsDetailsModal.jsx
+++ src/news/NewsDetailsModal.jsx
@@ -1,21 +1,18 @@
-import React, { forwardRef, useImperativeHandle, useState, useEffect, } from 'react';
-import {
-  StandardModal, useToggle, Container
-} from '@openedx/paragon';
+import React, { forwardRef, useImperativeHandle, useState, useEffect } from 'react';
+import { StandardModal, useToggle, Container } from '@openedx/paragon';
 import './News.scss';
 import './NewsDetailsModal.scss';
 import NewsBody from './NewsBody';
 
-const NewsDetailModal = forwardRef(({base}, ref) => {
+const NewsDetailModal = forwardRef(({ base }, ref) => {
   const [isOpen, open, close] = useToggle(false);
   const [content, setContent] = useState('');
   const [slug, setSlug] = useState('');
-  
 
   useImperativeHandle(ref, () => ({
     openModal(openSlug) {
-      window.history.replaceState({}, '', `/home/news/${openSlug}`);
-      setSlug(openSlug)
+      window.history.pushState({ modalOpen: true }, '', `/home/news/${openSlug}`);
+      setSlug(openSlug);
       open();
     },
   }));
@@ -23,32 +20,48 @@ const NewsDetailModal = forwardRef(({base}, ref) => {
   const onClose = () => {
     window.history.replaceState({}, '', `${base}`);
     close();
-  }
+  };
 
   const fetchNews = async () => {
+    if (!slug) return;
     try {
       const module = await import(`!raw-loader!../news/news/${slug}.md`);
       setContent(module.default);
     } catch (err) {
-    console.log(err)
+      console.log(err);
     }
   };
 
-   useEffect(() => {
-     fetchNews();
-     console.log(slug)
-   }, [slug]);
+  useEffect(() => {
+    fetchNews();
+  }, [slug]);
+
+  // Listen for browser back/forward navigation
+  useEffect(() => {
+    const handlePopState = () => {
+      if (isOpen) {
+        close();
+      }
+    };
+
+    window.addEventListener('popstate', handlePopState);
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, [isOpen, close]);
 
   return (
-    <div >
+    <div>
       <StandardModal
-        className='news-details-modal'
+        className="news-details-modal"
         title=""
         isOpen={isOpen}
         onClose={onClose}
         isOverflowVisible={false}
       >
-        <Container size="md"><NewsBody content={content} /></Container>
+        <Container size="md">
+          <NewsBody content={content} />
+        </Container>
       </StandardModal>
     </div>
   );
