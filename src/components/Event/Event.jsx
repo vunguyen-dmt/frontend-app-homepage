20a52e8c849b14b75f80ecc955ff8cc9d8051c19
Vu Nguyen <vu.nguyen@dmttek.com>
2025-08-09 23:54:50 +0700

    replace dummy events

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -2,32 +2,32 @@ import React from 'react';
 import './Event.scss';
 
 const Event = () => {
-    var eventList = [
+    var events = [
         {
             id: 1,
-            eventName: 'Khóa học trực tuyến mở rộng MOOC “Nữ giới và năng lực lãnh đạo” sẽ chính thức khởi động vào 20/7 tới',
-            date: new Date(2025,6,17),
+            eventName: 'Hướng dẫn sử dụng hệ thống HUTECH eLearning cho tân sinh viên khóa 2025.',
+            date: '01/09/2025   ',
         },
         {
             id: 2,
-            eventName: 'Khóa học trực tuyến mở rộng MOOC “Nữ giới và năng lực lãnh đạo” sẽ chính thức khởi động vào 20/7 tới',
-            date: new Date(2025,6,17),
+            eventName: 'Tập huấn vận hành khóa học điện tử trên hệ thống Hutech eLearning - HK1/ 2025-2026 dành cho Giảng viên.',
+            date: '23/08/2025 08:30',
         },
         {
             id: 3,
-            eventName: 'Khóa học trực tuyến mở rộng MOOC “Nữ giới và năng lực lãnh đạo” sẽ chính thức khởi động vào 20/7 tới',
-            date: new Date(2025,6,17),
+            eventName: 'Hướng dẫn quay - dựng video bài giảng điện tử dành cho giảng viên.',
+            date: '15/08/2025 14:30',
         },
     ];
 
     return (
         <>
         {
-            eventList.map(i => (
+            events.map(i => (
                 <div key={i.id}>
                     <div className="d-flex flex-column flex-lg-row align-items-start align-items-lg-center event-item">
-                        <div className="event-date text-left pr-lg-3">{i.date.toLocaleDateString("vi-VN")}</div>
-                        <div className="event-name pl-lg-3 text-justify">{i.eventName}</div>
+                        <div className="event-date text-left pr-lg-3">{i.date.toString("dd/MM/yyyy")}</div>
+                        <div className="event-name pl-lg-3">{i.eventName}</div>
                     </div>
                     <div className="event-line"></div>
                 </div>
